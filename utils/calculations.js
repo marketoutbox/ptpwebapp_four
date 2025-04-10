3530e8d14e2712e3ce863f634c3162174087bb23
marketoutbox <marketoutbox@yahoo.com>
2025-04-10 05:58:16 +0530

    Delete utils directory

--- utils/calculations.js
+++
@@ -1,5 +0,0 @@
-export default function calculateZScore(values) {
-  const mean = values.reduce((sum, val) => sum + val, 0) / values.length
-  const stdDev = Math.sqrt(values.reduce((sum, val) => sum + Math.pow(val - mean, 2), 0) / values.length)
-  return values.map((val) => (val - mean) / stdDev)
-}
